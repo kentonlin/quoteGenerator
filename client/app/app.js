fc7e71e702ceab8c0b99784b7e6906228f346920
Kenton <kentonlin11@gmail.com>
2016-07-06 14:18:12 -0400

    MVP WORKING adding route now

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,49 @@
-console.log('in here');
 angular.module("App", [])
-.controller("myController", function($scope) {
-	$scope.data = "hello"; 
+.controller("myController", function($scope,appQuote) {
+	$scope.data = {};
+	$scope.quote = function() {
+		console.log('quote method active');
+		appQuote.getQuote()
+			.then(function(quo) {
+				$scope.data.quo = quo;
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
+})
+.factory("appQuote", function($http) {
 
+		var array = []; 
+
+		var getQuote = function() {
+		return $http({
+			url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+  		method: "POST",
+  		headers: {
+    		"content-type": "application/x-www-form-urlencoded",
+    		"accept": "application/json",
+    		"x-mashape-key": "85undWA8fnmsh09irl8YW1g0r816p1chfHnjsnxLekODfILfup"
+    	}
+		})
+		.then(function(resp) {
+			array.push(resp.data.quote);
+			return resp.data.quote;
+		})
+	}
+
+	// var getQuote = function() {
+	// 	return $http({
+	// 		method: 'POST',
+	// 		url: 'http://quotes.rest/qod.json'
+	// 	})
+	// 	.then(function(resp) {
+	// 		console.log(resp.data.contents.quotes[0].quote)
+	// 		return resp.data.contents.quotes[0].quote;
+	// 	})
+	// }
+
+	return {
+		getQuote: getQuote
+	}
 })
