560a6e982d62143e5dc67e163f4e9c7a651722d0
Kenton <kentonlin11@gmail.com>
2016-07-06 17:21:05 -0400

    port change

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/../client'))
 
-app.listen(3000, function(error) {
+app.listen(process.env.PORT || 3000, function(error) {
 	console.log('dirname is', __dirname)
 	console.log("server is listening on port 3000")
 });
